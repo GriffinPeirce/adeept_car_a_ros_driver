b83bf26ec4b3a7f4f4ff034805f754ccf3ea25d7
GriffinPeirce <griff.peirce@gmail.com>
2019-07-08 14:53:01 -0700

    fix angular velocity revsered

--- scripts/awr_startup.sh
+++ scripts/awr_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
-roslaunch ~/catkin_ws/src/adeept_awd_ros_driver/launch/awd.launch
+roslaunch ~/catkin_ws/src/adeept_awr_ros_driver/launch/awr.launch
