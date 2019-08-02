e287316ba6a3e893c4c1f528688cb3a56bd0b36e
GriffinPeirce <griff.peirce@gmail.com>
2019-08-02 21:35:30 +0000

    First driving

--- scripts/car_startup.sh
+++ scripts/car_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
 source /home/ubuntu/catkin_ws/devel/setup.bash
-roslaunch ~/catkin_ws/src/adeept_awr_ros_driver/launch/awr.launch
+roslaunch ~/catkin_ws/src/adeept_car_a_ros_driver/launch/car.launch
--- scripts/driver.py
+++ scripts/driver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-import move
+import motor
 import rospy
 import time
 import ultra
@@ -9,9 +9,8 @@ import ultra
 from std_msgs.msg import String, Bool, Header
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Range
-from adeept_awr_ros_driver.msg import ArrayIR
 
-class AdeeptAWRRos():
+class AdeeptCarRos():
 
 	def __init__(self):
 		self._lin_vel = 0
@@ -19,35 +18,17 @@ class AdeeptAWRRos():
 		self._cmd_lin_vel = 0
 		self._cmd_ang_vel = 0
 		self._wheel_vel = (0,0)
-		self._axle_length = 0.155
-		self._wheel_radius = 0.070
+		self._axle_length = 0.11
+		self._wheel_radius = 0.025
 		self._left_motor_dir = 1
 		self._right_motor_dir = 0
-		self._line_pin_right = 35
-		self._line_pin_middle = 36
-		self._line_pin_left = 38
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(self._line_pin_right,GPIO.IN)
-		GPIO.setup(self._line_pin_middle,GPIO.IN)
-		GPIO.setup(self._line_pin_left,GPIO.IN)
-		move.setup()
-		move.motorStop()
+		motor.setup()
+		motor.motorStop()
 
 		self._twist_sub = rospy.Subscriber('cmd_vel', Twist, self._twist_callback, queue_size=1)
 		self._sonar_pub = rospy.Publisher('sonar', Range, queue_size = 1)
-		self._IR_pub = rospy.Publisher('ir', ArrayIR, queue_size = 1)
-
-	def _publish_ir(self):
-		if self._IR_pub.get_num_connections() == 0:
-			return
-		now = rospy.Time.now()
-		ir = ArrayIR()
-		ir.header.stamp = now
-		ir.left = GPIO.input(self._line_pin_left)
-		ir.middle = GPIO.input(self._line_pin_middle)
-		ir.right = GPIO.input(self._line_pin_right)
-		self._IR_pub.publish(ir)
 
 	def _publish_sonar(self):
 
@@ -72,39 +53,38 @@ class AdeeptAWRRos():
 
 		# Disable both motors
 		if self._cmd_lin_vel == 0 and self._cmd_ang_vel == 0:
-			move.motorStop()
+			motor.motorStop()
 		else:
 			# Forward driving
 			if self._cmd_lin_vel > 0:
 				self._left_motor_dir = 1
-				self._right_motor_dir = 0
+				self._right_motor_dir = 1
 			# Reverse driving
 			elif self._cmd_lin_vel < 0:
 				self._left_motor_dir = 0
-				self._right_motor_dir = 1
+				self._right_motor_dir = 0
 			# CCW Rotation
 			elif self._cmd_ang_vel < 0:
-				self._left_motor_dir = 0
+				self._left_motor_dir = 1
 				self._right_motor_dir = 0
 			# CW Rotation
 			elif self._cmd_ang_vel > 0:
-				self._left_motor_dir = 1
+				self._left_motor_dir = 0
 				self._right_motor_dir = 1
-			move.motor_left(1,self._left_motor_dir,100)
-			move.motor_right(1,self._right_motor_dir,100)
+			motor.motor1(1,self._left_motor_dir,100)
+			motor.motor(1,self._right_motor_dir,100)
 
 	def run(self):
-		rospy.init_node('adeept_awr_driver')
+		rospy.init_node('adeept_car_driver')
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
 			self._publish_sonar()
-			self._publish_ir()
 			rate.sleep()
-		move.destroy()
+		motor.destroy()
 
 if __name__ == '__main__':
     try:
-        awr = AdeeptAWRRos()
-        awr.run()
+        car = AdeeptCarRos()
+        car.run()
     except rospy.ROSInterruptException as e:
         sys.exit('Connection Error: {}'.format(e))
